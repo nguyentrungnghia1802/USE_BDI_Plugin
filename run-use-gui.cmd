76ae7fd11f973f7b30fe2d9c6dbb128bd4bd05b5
Nguyen Trung Nghia <trungnghia180205@gmail.com>
2026-08-07 01:03:25 +0700

    chore(scripts): auto-copy bdi plugin jar to use-gui plugin folder before launch

--- run-use-gui.cmd
+++ run-use-gui.cmd
@@ -1,5 +1,7 @@
 @echo off
 cd /d "%~dp0"
+if not exist "use-gui\lib\plugins" mkdir "use-gui\lib\plugins"
+if exist "use-bdi-plugin\target\use-bdi-plugin-7.1.1.jar" copy /y "use-bdi-plugin\target\use-bdi-plugin-7.1.1.jar" "use-gui\lib\plugins\" >nul
 if defined JAVA_HOME (
     "%JAVA_HOME%\bin\java.exe" -jar "use-gui\target\use-gui.jar" -nr
 ) else (
--- run-use-gui.ps1
+++ run-use-gui.ps1
@@ -1,3 +1,7 @@
 Set-Location $PSScriptRoot
+New-Item -ItemType Directory -Force -Path "use-gui\lib\plugins" | Out-Null
+if (Test-Path "use-bdi-plugin\target\use-bdi-plugin-7.1.1.jar") {
+    Copy-Item -Force "use-bdi-plugin\target\use-bdi-plugin-7.1.1.jar" "use-gui\lib\plugins\"
+}
 $javaCmd = if ($env:JAVA_HOME) { "$env:JAVA_HOME\bin\java.exe" } else { "java" }
 & $javaCmd -jar "use-gui\target\use-gui.jar" -nr
